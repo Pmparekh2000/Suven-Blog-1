6080f9268a8f48ba00f746e292482e00f2f40acb
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-09-05 06:44:12 -0700

    Added class views

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
     # If we pass <parameter> then this parameter is captured as a string, but to make it an integer, we provide path converters
     

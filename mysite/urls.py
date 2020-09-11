fadaaccaab5960a4f5a26db29800704870ff5f82
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-09-11 10:27:40 -0700

    Adding to Heroku

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('/', HttpResponse("Hello World")),
     path('blog/', include('blog.urls', namespace='blog'))
 ]
